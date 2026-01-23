af5badd9ee99e483e3ee34a148e94fe3b3d4c0d7
Adeel-vtx <adeelvex881@gmail.com>
2026-01-23 12:01:09 +0500

    correct seeding

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
     role = db.Column(db.String(20), nullable=False, default='member') # member or admin
     is_blocked = db.Column(db.Boolean, default=False)
 
